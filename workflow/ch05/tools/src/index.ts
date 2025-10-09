7ea306ac844fd8e1213f26835bc71d2805a5526f
mech-master <2906465@gmail.com>
2025-10-09 16:31:01 +0300

    Page 177. Jest testing Typescript

--- workflow/ch05/tools/src/index.ts
+++ workflow/ch05/tools/src/index.ts
@@ -1,5 +1,10 @@
-function printMessage(msg:string):void {
-    console.log(`Message: ${msg}`);
-}
+import { sum } from "./calc.js";
 
-printMessage('Hello, TypeScript');
+let printMessage = (msg:string):void => console.log(`Message: ${msg}`);
+
+let message = ("Hello, TypeScript");
+printMessage(message);
+
+let total = sum(100, 200, 300);
+console.log(`Total: ${total}`);
+
---
+++ workflow/ch06/tools/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    "ignores": [
+        "node_modules/**",
+        "dist/**"
+    ]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "prefer-const": "off",
+      semi: ["error", "always"],
+    }
+  },
+]);
